91a6af46f07b3b3cb2bde6194d7774266b75b478
Liam <86498790+Liam-0001@users.noreply.github.com>
2026-05-11 12:55:29 +0200

    3D variant UI update (#54)
    * bot
    
    * add name to bots
    
    * add colors to selection and player dashboard
    
    * export
    
    * export
    
    ---------
    
    Co-authored-by: Liam Omen <liam.sp@hotmail.com>
    Co-authored-by: Hendrik de Jonge <hendrikdejonge@hotmail.com>

--- Game/UINode.cs
+++ Game/UINode.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -12,229 +12,261 @@ namespace TankDestroyer;
 
 public partial class UINode : Control
 {
-    [Export] public Button StartButton { get; set; }
-    [Export] public Button PlayButton { get; set; }
-    [Export] public Button NextButton { get; set; }
-    [Export] public Button PreviousButton { get; set; }
-    [Export] public Button PauseButton { get; set; }
-    [Export] public OptionButton MapButton { get; set; }
-    [Export] public Control PlayersContainer { get; set; }
-    [Export] public Control PlayerScoresContainer { get; set; }
-    [Export] public GameNode GameNode { get; set; }
-    [Export] public HSlider TurnSlider { get; set; }
+	[Export] public Button StartButton { get; set; }
+	[Export] public Button PlayButton { get; set; }
+	[Export] public Button NextButton { get; set; }
+	[Export] public Button PreviousButton { get; set; }
+	[Export] public Button PauseButton { get; set; }
+	[Export] public OptionButton MapButton { get; set; }
+	[Export] public Control PlayersContainer { get; set; }
+	[Export] public Control PlayerScoresContainer { get; set; }
+	[Export] public GameNode GameNode { get; set; }
+	[Export] public HSlider TurnSlider { get; set; }
 
-    public List<BotSelection> BotSelections { get; set; } = new();
+	public List<BotSelection> BotSelections { get; set; } = new();
 
-    public Type[] BotTypes { get; set; }
-    public World[] Maps { get; set; }
+	public Type[] BotTypes { get; set; }
+	public World[] Maps { get; set; }
 
-    public int PlayerCount { get; set; }
-    private Dictionary<int, Label> _tankHealthMapping = new();
-    private readonly ICollectBotService _collectBotService = new CollectBotsService();
-    private readonly ICollectMapsService _collectMapsService = new CollectMapsService();
-
-
-    public override void _Ready()
-    {
-        ConfigFile file =
-            System.Text.Json.JsonSerializer.Deserialize<ConfigFile>(System.IO.File.ReadAllText("config.json"));
-        BotTypes = _collectBotService.LoadBots(Path.GetFullPath(file.BotFolder));
-        Maps = _collectMapsService.LoadMaps(Path.GetFullPath(file.MapFolder));
-        base._Ready();
-        MapButton.AddItem("Random generated", 0);
-        for (int i = 0; i < Maps.Length; i++)
-        {
-            MapButton.AddItem(Maps[i].Name, i + 1);
-        }
-
-        MapButton.ItemSelected += MapButtonOnItemSelected;
-        MapButtonOnItemSelected(0);
-
-        StartButton.Pressed += StartButtonOnPressed;
-        PauseButton.Pressed += PauseButtonPressed;
-        PlayButton.Pressed += PlayButtonPressed; /*        TurnSlider.MaxValue = _gameRunner.GetTurns().Length;
-        TurnSlider.Value = TurnSlider.MaxValue;*/
-        GameNode.ChangedTurn += OnGameNodeChangedTurn;
-
-        NextButton.Pressed += NextButtonOnPressed;
-        PreviousButton.Pressed += PreviousButtonOnPressed;
-
-        TurnSlider.ValueChanged += TurnSliderOnValueChanged;
-    }
+	public int PlayerCount { get; set; }
+	private Dictionary<int, Label> _tankHealthMapping = new();
+	private readonly ICollectBotService _collectBotService = new CollectBotsService();
+	private readonly ICollectMapsService _collectMapsService = new CollectMapsService();
 
 
-    private void TurnSliderOnValueChanged(double value)
-    {
-        if (GameNode.IsPlaying())
-        {
-            return;
-        }
-        else
-        {
-            GameNode.SetStepIndex((int)value);
-        }
-    }
+	public override void _Ready()
+	{
+		ConfigFile file =
+			System.Text.Json.JsonSerializer.Deserialize<ConfigFile>(System.IO.File.ReadAllText("config.json"));
+		BotTypes = _collectBotService.LoadBots(Path.GetFullPath(file.BotFolder))
+			.OrderBy(botType => botType.GetCustomAttribute<BotAttribute>()?.Name)
+			.ToArray();
+		Maps = _collectMapsService.LoadMaps(Path.GetFullPath(file.MapFolder));
+		base._Ready();
+		MapButton.AddItem("Random generated", 0);
+		for (int i = 0; i < Maps.Length; i++)
+		{
+			MapButton.AddItem(Maps[i].Name, i + 1);
+		}
 
-    private void PreviousButtonOnPressed()
-    {
-        if (GameNode.IsPlaying())
-        {
-            return;
-        }
+		MapButton.ItemSelected += MapButtonOnItemSelected;
+		MapButtonOnItemSelected(0);
 
-        GameNode.StepBack();
-    }
+		StartButton.Pressed += StartButtonOnPressed;
+		PauseButton.Pressed += PauseButtonPressed;
+		PlayButton.Pressed += PlayButtonPressed; /*        TurnSlider.MaxValue = _gameRunner.GetTurns().Length;
+		TurnSlider.Value = TurnSlider.MaxValue;*/
+		GameNode.ChangedTurn += OnGameNodeChangedTurn;
 
-    private void NextButtonOnPressed()
-    {
-        if (GameNode.IsPlaying())
-        {
-            return;
-        }
+		NextButton.Pressed += NextButtonOnPressed;
+		PreviousButton.Pressed += PreviousButtonOnPressed;
 
-        GameNode.DoTurn();
-    }
+		TurnSlider.ValueChanged += TurnSliderOnValueChanged;
+	}
 
-    private void PauseButtonPressed()
-    {
-        GameNode.PausePlay();
-    }
 
-    private void OnGameNodeChangedTurn()
-    {
-        TurnSlider.MaxValue = GameNode.GetRunner().GetTurns().Length - 1;
-        TurnSlider.Value = GameNode.GetCurrentTurnIndex();
-    }
+	private void TurnSliderOnValueChanged(double value)
+	{
+		if (GameNode.IsPlaying())
+		{
+			return;
+		}
+		else
+		{
+			GameNode.SetStepIndex((int)value);
+		}
+	}
 
-    public override void _PhysicsProcess(double delta)
-    {
-        base._PhysicsProcess(delta);
-    }
+	private void PreviousButtonOnPressed()
+	{
+		if (GameNode.IsPlaying())
+		{
+			return;
+		}
 
-    private void PlayButtonPressed()
-    {
-        GameNode.StartPlay();
-    }
+		GameNode.StepBack();
+	}
 
-    public override void _Process(double delta)
-    {
-        base._Process(delta);
-        if (!GameNode.HasGame || _tankHealthMapping.Keys.Count <= 0)
-        {
-            return;
-        }
+	private void NextButtonOnPressed()
+	{
+		if (GameNode.IsPlaying())
+		{
+			return;
+		}
 
-        var players = GameNode.GetPlayers();
-        for (int i = 0; i < players.Length; i++)
-        {
-            var player = players[i];
-            _tankHealthMapping[player.Tank.OwnerId]?.Text = player.Tank.Health + "%";
-        }
-    }
+		GameNode.DoTurn();
+	}
 
-    private void StartButtonOnPressed()
-    {
-        _tankHealthMapping.Clear();
-        List<IPlayerBot> bots = new();
+	private void PauseButtonPressed()
+	{
+		GameNode.PausePlay();
+	}
 
-        for (int i = 0; i < PlayerCount; i++)
-        {
-            if (BotSelections[i].SelectedBot == null)
-            {
-                continue;
-            }
+	private void OnGameNodeChangedTurn()
+	{
+		TurnSlider.MaxValue = GameNode.GetRunner().GetTurns().Length - 1;
+		TurnSlider.Value = GameNode.GetCurrentTurnIndex();
+	}
 
-            bots.Add((IPlayerBot)Activator.CreateInstance(BotSelections[i].SelectedBot));
-        }
+	public override void _PhysicsProcess(double delta)
+	{
+		base._PhysicsProcess(delta);
+	}
 
-        World world = null;
-        if (MapButton.Selected == 0)
-        {
-            world = World.GenerateRandom(20, 20);
-        }
-        else
-        {
-            world = Maps[MapButton.Selected - 1];
-        }
-        var runner = new GameRunner(world, bots.ToArray());
-        GameNode.StartGame(runner);
+	private void PlayButtonPressed()
+	{
+		GameNode.StartPlay();
+	}
 
-        PlayerScoresContainer.ClearChilderen();
-        foreach (var player in GameNode.GetPlayers())
-        {
-            PlayerScoresContainer.AddChild(new Label()
-            {
-                Text = player.Name
-            });
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
+		if (!GameNode.HasGame || _tankHealthMapping.Keys.Count <= 0)
+		{
+			return;
+		}
 
-            PlayerScoresContainer.AddChild(new Label()
-            {
-                Text = player.Creator
-            });
+		var players = GameNode.GetPlayers();
+		for (int i = 0; i < players.Length; i++)
+		{
+			var player = players[i];
+			_tankHealthMapping[player.Tank.OwnerId]?.Text = player.Tank.Health + "%";
+		}
+	}
 
-            var healthLabel = new Label()
-            {
-                Text = player.Tank.Health + "%"
-            };
-            PlayerScoresContainer.AddChild(healthLabel);
-            _tankHealthMapping.Add(player.Tank.OwnerId, healthLabel);
-        }
-    }
+	private void StartButtonOnPressed()
+	{
+		_tankHealthMapping.Clear();
+		List<IPlayerBot> bots = new();
 
-    private void MapButtonOnItemSelected(long index)
-    {
-        if (index == 0)
-        {
-            PlayerCount = 2;
-        }
-        else
-        {
-            PlayerCount = Maps[index - 1].SpawnPoints.Length;
-        }
+		for (int i = 0; i < PlayerCount; i++)
+		{
+			if (BotSelections[i].SelectedBot == null)
+			{
+				continue;
+			}
 
-        while (BotSelections.Count < PlayerCount)
-        {
-            BotSelections.Add(new BotSelection());
-        }
+			bots.Add((IPlayerBot)Activator.CreateInstance(BotSelections[i].SelectedBot));
+		}
 
-        InitializeBotSelection();
-    }
+		World world = null;
+		if (MapButton.Selected == 0)
+		{
+			world = World.GenerateRandom(20, 20);
+		}
+		else
+		{
+			world = Maps[MapButton.Selected - 1];
+		}
+		var runner = new GameRunner(world, bots.ToArray());
+		GameNode.StartGame(runner);
 
-    private void InitializeBotSelection()
-    {
-        PlayersContainer.ClearChilderen();
-        for (int i = 0; i < PlayerCount; i++)
-        {
-            var botSelection = BotSelections[i];
-            PlayersContainer.AddChild(new Label() { Text = (i + 1).ToString() });
-            OptionButton optionButton = new();
-            optionButton.AddItem("<geen>");
+		PlayerScoresContainer.ClearChilderen();
+		foreach (var player in GameNode.GetPlayers())
+		{
+			var hexColor = player.Color;
 
-            foreach (var botType in BotTypes)
-            {
-                optionButton.AddItem(botType.GetCustomAttribute<BotAttribute>()?.Name);
-            }
+			var nameLabel = new Label()
+			{
+				Text = player.Name,
+			};
 
-            optionButton.ItemSelected += index =>
-            {
-                if (index == 0)
-                {
-                    botSelection.SelectedBot = null;
-                    return;
-                }
+			var creatorLabel = new Label()
+			{
+				Text = player.Creator
+			};
 
-                botSelection.SelectedBot = BotTypes[index - 1];
-            };
+			var healthLabel = new Label()
+			{
+				Text = player.Tank.Health + "%"
+			};
 
-            if (botSelection.SelectedBot == null)
-            {
-                optionButton.Selected = 0;
-            }
-            else
-            {
-                optionButton.Selected = BotTypes.IndexOf(botSelection.SelectedBot) + 1;
-            }
-            PlayersContainer.AddChild(optionButton);
-        }
-    }
+			var color = Color.FromHtml(hexColor);
+			
+			nameLabel.AddThemeColorOverride("font_color", color);
+			creatorLabel.AddThemeColorOverride("font_color", color);
+			healthLabel.AddThemeColorOverride("font_color", color);
+			
+			PlayerScoresContainer.AddChild(nameLabel);
+			PlayerScoresContainer.AddChild(creatorLabel);
+			PlayerScoresContainer.AddChild(healthLabel);
+			_tankHealthMapping.Add(player.Tank.OwnerId, healthLabel);
+		}
+	}
+
+	private void MapButtonOnItemSelected(long index)
+	{
+		if (index == 0)
+		{
+			PlayerCount = 2;
+		}
+		else
+		{
+			PlayerCount = Maps[index - 1].SpawnPoints.Length;
+		}
+
+		while (BotSelections.Count < PlayerCount)
+		{
+			BotSelections.Add(new BotSelection());
+		}
+
+		InitializeBotSelection();
+	}
+
+	private void InitializeBotSelection()
+	{
+		PlayersContainer.ClearChilderen();
+		for (int i = 0; i < PlayerCount; i++)
+		{
+			var botSelection = BotSelections[i];
+			var playerIndex = new Label() { Text = (i + 1).ToString() };
+			
+			PlayersContainer.AddChild(playerIndex);
+			OptionButton optionButton = new();
+			optionButton.AddItem("<geen>");
+
+			if (botSelection.SelectedBot == null)
+			{
+				optionButton.AddThemeColorOverride("font_color", Colors.White);
+			}
+			else
+			{
+				var hexColor =botSelection.SelectedBot.GetCustomAttribute<BotAttribute>()?.Color;
+				optionButton.AddThemeColorOverride("font_color", Color.FromHtml(hexColor));
+			}
+			
+			foreach (var botType in BotTypes)
+			{
+				var name = botType.GetCustomAttribute<BotAttribute>()?.Name;
+				var creator = botType.GetCustomAttribute<BotAttribute>()?.Creator;
+				
+				var label = $"{name} ({creator})";
+				optionButton.AddItem(label);
+			}
+
+			optionButton.ItemSelected += index =>
+			{
+				if (index == 0)
+				{
+					botSelection.SelectedBot = null;
+					optionButton.AddThemeColorOverride("font_color", Colors.White);
+					return;
+				}
+
+				botSelection.SelectedBot = BotTypes[index - 1];
+				var hexColor =botSelection.SelectedBot.GetCustomAttribute<BotAttribute>()?.Color;
+				optionButton.AddThemeColorOverride("font_color", Color.FromHtml(hexColor));
+			};
+
+			if (botSelection.SelectedBot == null)
+			{
+				optionButton.Selected = 0;
+			}
+			else
+			{
+				optionButton.Selected = BotTypes.IndexOf(botSelection.SelectedBot) + 1;
+			}
+			PlayersContainer.AddChild(optionButton);
+		}
+	}
 }
